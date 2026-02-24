9230247de032c451821d38ce90a0f104f43018c5
ghrd1 <romanik.danil@gmail.com>
2026-02-24 23:00:13 +0200

    zxc

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,16 +1,16 @@
 # Be sure to restart your server when you modify this file.
 
-# Avoid CORS issues when API is called from the frontend app.
-# Handle Cross-Origin Resource Sharing (CORS) in order to accept cross-origin Ajax requests.
-
-# Read more: https://github.com/cyu/rack-cors
-
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "http://localhost", "http://localhost:3001", "http://localhost:5173"
+    # Добавил адрес твоего фронтенда на Render
+    origins "http://localhost", 
+            "http://localhost:3001", 
+            "http://localhost:5173", 
+            "https://shop-front-0740.onrender.com"
 
     resource "*",
       headers: :any,
+      expose: ["Authorization"], # Важно для JWT
       methods: [:get, :post, :put, :patch, :delete, :options, :head],
       credentials: true
   end
