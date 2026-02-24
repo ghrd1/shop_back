3b718ce7b74a68a6205a65c068e636e2db58043f
ghrd1 <romanik.danil@gmail.com>
2026-02-24 22:37:30 +0200

    rty

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,20 @@
+# Создание админа
+if User.count.zero?
+  puts "Создаём тестового админа..."
+  User.create!(
+    email: 'admin@test.com',
+    password: 'password',
+    password_confirmation: 'password',
+    role: 'admin',
+    first_name: 'Admin',
+    last_name: 'User'
+  )
+  puts "Админ создан: admin@test.com / password"
+else
+  puts "Пользователи уже есть, пропускаем создание админа..."
+end
+
+# Твои товары
 if Item.count.zero?
   puts "Создаём тестовые товары..."
   10.times do |i|
@@ -7,6 +24,7 @@ if Item.count.zero?
       price: rand(100..1000)
     )
   end
+  puts "Товары созданы!"
 else
   puts "Товары уже есть, пропускаем..."
 end
